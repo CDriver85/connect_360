f41af3b8a5a8acbb2533f25be25487cfac0932c6
Clayton Driver <Clayton.driver@gmail.com>
2020-07-30 04:12:31 +0000

    imported parent ln 2

--- users/models.py
+++ users/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from .models import Parent
 
 # Create your models here.
+
 class Parent(models.Model):
     name = models.CharField
-    (max_length =225)
+    (max_length=225)
     email = models.CharField
-    (max_length =225)
+    (max_length=225)
     phone = models.CharField
-    (max_length225)
+    (max_length=225)
     school = models.CharField
-    (max_length 225)
-    number of devices = models. CharField(max_length 225)
+    (max_length=225)
+    number_of_devices = models.CharField(max_length=225)
 
 
 
