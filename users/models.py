0ba9064c8df77ac16e4a4a6a7ff6f3f2cdb1c905
Cassandra  Severin <cmseverin01@gmail.com>
2020-07-30 00:40:16 +0000

    completed the donor models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Donor(models.Model):
+    name = models.charField(max_length =255)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=11,
+                                    validators=[phone_regex],
+                                    null=True,
+                                    blank=True)
+
+
+ def __str__(Donor):
+    return f"{Donor.text}"
