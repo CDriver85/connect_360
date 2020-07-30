985ef2e34c8445f0220fb946a2c4dd6c81e00cfe
Clayton Driver <Clayton.driver@gmail.com>
2020-07-30 04:05:43 +0000

    added correct syntax

--- connect360/views.py
+++ connect360/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import. django.form.forms
+form.models import form
 
 # Create your views here.
+class form(form.ModelForm):
+    class meta:
+        model = form
+        fields = [
+            'name',
+            'email',
+            'phone',
+            'school',
+            'number_of_devices',
+        ]
+
+
+
+
+# # from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+
+# admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Parent(models.Model):
+    name = models.CharField
+    (max_length =225)
+    email = models.CharField
+    (max_length =225)
+    phone = models.CharField
+    (max_length225)
+    school = models.CharField
+    (max_length 225)
+    number of devices = models. CharField(max_length 225)
+
+
+
+
