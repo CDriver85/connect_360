b18484983ad4d4fba1e58e77bc0bed3360d69b0e
Cassandra  Severin <cmseverin01@gmail.com>
2020-07-30 03:21:02 +0000

    added donor info to views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from .models import Donor
+from .forms import DonorForm
 
-# Create your views here.
+def Donor(request):
+    donors = Donor.objects.all()
+    if request.method == 'GET':
+        form = DonorForm()
+    else:
+        form = DonorForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='donors')
+    return render(request, "donors/donors.html",
+                  {"donors": donors,'form':form})
