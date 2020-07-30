10b14aa5bd81aa614f5b9b2d67f3e5047b32fe07
Clayton Driver <Clayton.driver@gmail.com>
2020-07-30 15:34:49 +0000

    created parent_form

--- connect360/views.py
+++ connect360/views.py
@@ -1,19 +1,3 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-class form(form.ModelForm):
-    class meta:
-        model = form
-        fields = [
-            'name',
-            'email',
-            'phone',
-            'school',
-            'number_of_devices',
-        ]
-
-
 
 
 # # from django.contrib import admin
