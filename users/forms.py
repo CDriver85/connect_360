67dc0f17f50e1f8c0eaf9e3f0015c8beda85c168
Cassandra  Severin <cmseverin01@gmail.com>
2020-08-02 15:12:01 +0000

    updated donor request form.

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Donor
 
-class DonorForm(forms.ModelForm):
+class DonorRequestForm(forms.ModelForm):
     class Meta:
         model = Donor
         fields = [
             'name',
             'phone_number',
             'email',
+            'number_of_devices',
+            'school',
+
+]
+
+
 
