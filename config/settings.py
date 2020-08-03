5d3b68105337449d72677697588954888fade3f8
Genola Johnson <genolajohnson@gmail.com>
2020-08-03 00:08:43 +0000

    Updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -61,8 +61,6 @@ TEMPLATES = [
 
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
 
-        'DIRS': [],
-
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
