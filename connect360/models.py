aed1af7e44a3aaf5d4001d8a4a1d1dd3d6fbf721
Genola Johnson <genolajohnson@gmail.com>
2020-08-03 16:58:32 +0000

    Uninstalled localflavor and updated fields for parent form in models

--- connect360/models.py
+++ connect360/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from localflavor.us.models import USStateField, USZipCodeField
+
 
 
 # Create your models here.
@@ -19,9 +19,9 @@ class ParentRequest(models.Model):
 
     city = models.CharField(max_length=255, null=True, blank=True)
 
-    state = USStateField(null=True, blank=True)
+    state = models.CharField(max_length=255,default="Georgia")
 
-    zip_code = USZipCodeField(null=True, blank=True)
+    zip_code = models.CharField(max_length=10, null=True, blank=True)
 
     phone = models.CharField(
         max_length=11, validators=[phone_regex], null=True, blank=True)
