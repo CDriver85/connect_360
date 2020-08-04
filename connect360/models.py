93b226c344195dc3066acac49cc3bebae0323b52
Clayton Driver <Clayton.driver@gmail.com>
2020-08-04 13:04:43 +0000

    Add Donor Request in 360/nodels.py

--- connect360/models.py
+++ connect360/models.py
@@ -34,3 +34,35 @@ class ParentalRequest(models.Model):
 
     def __str__(self):
         return f"{self.text}"
+
+
+class DonorRequest(models.Model):       
+    phone_regex = RegexValidator(
+        regex=r'^\+?\d{10}$',
+        message="Phone number must be entered in the format: '+9999999999'.")
+
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    email = models.EmailField(max_length=255, null=True, blank=True)
+
+    address_1 = models.CharField(max_length=255, null=True, blank=True)
+
+    address_2 = models.CharField(max_length=255, null=True, blank=True)
+
+    city = models.CharField(max_length=255, null=True, blank=True)
+
+    state = USStateField(null=True, blank=True)
+
+    zip_code = USZipCodeField(null=True, blank=True)
+
+    phone = models.CharField(
+        max_length=11, validators=[phone_regex], null=True, blank=True)
+
+    school = models.CharField(max_length=255, null=True, blank=True)
+
+    number_of_devices = models.CharField(max_length=1, null=True, blank=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.text}"
