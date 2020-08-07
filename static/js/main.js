40ce55fba4412ada0821c70ebc309083deecad13
Genola Johnson <genolajohnson@gmail.com>
2020-08-07 12:10:54 +0000

    Nav bar working

--- static/js/main.js
+++ static/js/main.js
@@ -29,16 +29,3 @@ donorFormButton.addEventListener('click', function(event){
 
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   let btn = document.querySelector('.button'),
-//       loader = document.querySelector('.loader'),
-//       check = document.querySelector('.check');
-  
-//   btn.addEventListener('click', function () {
-//     loader.classList.add('active');    
-//   });
- 
-//   loader.addEventListener('animationend', function() {
-//     check.classList.add('active'); 
-//   });
-// });
